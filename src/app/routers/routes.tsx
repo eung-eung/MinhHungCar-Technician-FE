3c8604234a5f56c076910ab178eabaf3dc8d789e
eung-eung <boyvip5231@gmail.com>
2024-08-09 20:50:23 +0700

    update route

--- src/app/routers/routes.tsx
+++ src/app/routers/routes.tsx
@@ -1,6 +1,8 @@
 import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
 import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
-
+import HandshakeRoundedIcon from '@mui/icons-material/HandshakeRounded';
+import PersonPinCircleRoundedIcon from '@mui/icons-material/PersonPinCircleRounded';
+import BuildCircleRoundedIcon from '@mui/icons-material/BuildCircleRounded';
 export interface RouteType {
     icon: JSX.Element;
     title: string;
@@ -11,8 +13,20 @@ export interface RouteType {
 export const sidebarRoutes: RouteType[] = [
     {
         icon: < DragIndicatorIcon />,
-        title: "Xe cần kiểm tra",
+        title: "Kiểm tra kĩ thuật",
         link: "/",
+        children: [
+            {
+                icon: <PersonPinCircleRoundedIcon />,
+                title: "Cho khách hàng",
+                link: "/",
+            },
+            {
+                icon: <HandshakeRoundedIcon />,
+                title: "Cho đối tác",
+                link: "/cars",
+            },
+        ]
     },
     {
         icon: <LogoutOutlinedIcon />,
